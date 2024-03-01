98c836f1a3f38fdd856d596a52d97390592f0027
flutterrushikesh <flutterrushikesh@gmail.com>
2024-03-02 01:58:25 +0530

    Dependancies

---
+++ Day1/Assignment5/shadow_container/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Shadow Container",
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 25,
+            ),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Container(
+            width: 300,
+            height: 300,
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.red,
+                  blurRadius: 0,
+                  spreadRadius: 10,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
