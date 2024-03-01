8537cef6feb742ba40ffe9472ca588f991651b6b
flutterrushikesh <flutterrushikesh@gmail.com>
2024-03-02 01:07:00 +0530

    Complete

--- Day1/Assignment2/appbar_3icons/lib/main.dart
+++ Day1/Assignment2/appbar_3icons/lib/main.dart
@@ -9,9 +9,33 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-        title: const Text("Core2Web"),
-      )),
+        appBar: AppBar(
+          title: const Text(
+            "Core2Web",
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 25,
+            ),
+          ),
+          backgroundColor: const Color.fromRGBO(0, 139, 147, 0.7),
+          actions: const [
+            Icon(
+              Icons.favorite_outline_outlined,
+              size: 28,
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Icon(
+              Icons.notifications,
+              size: 28,
+            ),
+            SizedBox(width: 5),
+            Icon(Icons.account_circle_outlined, size: 28),
+            SizedBox(width: 5),
+          ],
+        ),
+      ),
     );
   }
 }
