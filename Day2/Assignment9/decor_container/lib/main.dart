1bf72efb8f4cbbee54fa88d288123ad2b7508b62
flutterrushikesh <flutterrushikesh@gmail.com>
2024-03-02 19:31:40 +0530

    Complete

--- Day2/Assignment9/decor_container/lib/main.dart
+++ Day2/Assignment9/decor_container/lib/main.dart
@@ -9,10 +9,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Container(
+            height: 200,
+            width: 350,
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 214, 99, 170),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+            ),
+            padding: const EdgeInsets.all(15),
+            child: const Text(
+              "Rushikesh Dhale",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 20,
+              ),
+            ),
+          ),
         ),
       ),
     );
