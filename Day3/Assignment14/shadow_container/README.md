e9fd44ce9c821557f21dabc0f651e3b9252d6533
flutterrushikesh <flutterrushikesh@gmail.com>
2024-03-04 15:36:55 +0530

    Dependancies

---
+++ Day3/Assignment14/shadow_container/README.md
@@ -0,0 +1,3 @@
+# shadow_container
+
+A new Flutter project.
---
+++ Day3/Assignment14/shadow_container/android/app/src/main/kotlin/com/example/shadow_container/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shadow_container
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
