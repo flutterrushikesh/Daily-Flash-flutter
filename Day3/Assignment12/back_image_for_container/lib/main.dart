73b1a097ee4607f43fce741e97e0437351c951c5
flutterrushikesh <flutterrushikesh@gmail.com>
2024-03-04 15:48:19 +0530

    Complete

--- Day3/Assignment12/back_image_for_container/lib/main.dart
+++ Day3/Assignment12/back_image_for_container/lib/main.dart
@@ -15,13 +15,27 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Container(
-              height: 200,
-              width: 200,
-              decoration: const BoxDecoration(
-                // backgroundBlendMode: BlendMode.dstOver,
-                color: Colors.amber,
+            height: 200,
+            width: 200,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaXgtKrAvsSpl0LkcmEM8x48awE-kZfRC4vA&s',
+                ),
+                fit: BoxFit.cover,
               ),
-              child: const Center(child: Text("Welcome..."))),
+              color: Colors.amber,
+            ),
+            child: const Center(
+              child: Text(
+                "Welcome...",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 19,
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
