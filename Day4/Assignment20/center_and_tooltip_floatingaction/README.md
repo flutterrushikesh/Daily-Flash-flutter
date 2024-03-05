5981adcd0b894ca3e91b9b6900e7012295c2bde1
flutterrushikesh <flutterrushikesh@gmail.com>
2024-03-06 00:28:34 +0530

    Complete

---
+++ Day4/Assignment20/center_and_tooltip_floatingaction/README.md
@@ -0,0 +1,3 @@
+# center_and_tooltip_floatingaction
+
+A new Flutter project.
---
+++ Day4/Assignment20/center_and_tooltip_floatingaction/android/app/src/main/kotlin/com/example/center_and_tooltip_floatingaction/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.center_and_tooltip_floatingaction
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
