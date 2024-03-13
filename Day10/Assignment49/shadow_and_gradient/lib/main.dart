b2f0cbe72d04023b323fb2888d6a660d4ecad652
flutterrushikesh <flutterrushikesh@gmail.com>
2024-03-13 22:19:24 +0530

    Complete

--- Day10/Assignment49/shadow_and_gradient/lib/main.dart
+++ Day10/Assignment49/shadow_and_gradient/lib/main.dart
@@ -9,10 +9,37 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Shadow & Gradient"),
+          backgroundColor: Colors.blue,
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.red,
+                  offset: Offset(
+                    10,
+                    10,
+                  ),
+                ),
+              ],
+              gradient: LinearGradient(
+                colors: [
+                  Colors.blue.shade400,
+                  Colors.purple,
+                ],
+                begin: Alignment.centerLeft,
+                end: Alignment.center,
+              ),
+            ),
+          ),
         ),
       ),
     );
