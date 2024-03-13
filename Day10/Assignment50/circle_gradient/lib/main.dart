6235508c7e95e57f7f622f327a753cbf647db806
flutterrushikesh <flutterrushikesh@gmail.com>
2024-03-13 22:32:20 +0530

    Complete

--- Day10/Assignment50/circle_gradient/lib/main.dart
+++ Day10/Assignment50/circle_gradient/lib/main.dart
@@ -10,6 +10,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text(
