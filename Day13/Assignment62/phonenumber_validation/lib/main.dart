22a2e9c688977ea8044170650f39d106e01abe20
flutterrushikesh <flutterrushikesh@gmail.com>
2024-04-01 22:45:51 +0530

    Complete

--- Day13/Assignment62/phonenumber_validation/lib/main.dart
+++ Day13/Assignment62/phonenumber_validation/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,8 +25,78 @@ class PhoneValidation extends StatefulWidget {
 }
 
 class _PhoneValidationState extends State {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  TextEditingController phoneController = TextEditingController();
+  String? phoneError;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Phone validation"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Form(
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: phoneController,
+                decoration: const InputDecoration(
+                  labelText: "Enter Phone no",
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a phone number';
+                  } else if (!RegExp(r'^[0-9]{10}$').hasMatch(value)) {
+                    return 'Please enter a valid 10-digit phone number';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 19),
+              SizedBox(
+                height: 50,
+                width: 150,
+                child: ElevatedButton(
+                  onPressed: () {
+                    bool validateTextFeilds = formKey.currentState!.validate();
+                    if (validateTextFeilds) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text("Validation passed"),
+                        ),
+                      );
+                    } else {
+                      setState(() {
+                        phoneError = phoneController.text.isEmpty
+                            ? 'PhoneNo is required'
+                            : null;
+                      });
+                    }
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.blue),
+                    foregroundColor: MaterialStatePropertyAll(Colors.black),
+                  ),
+                  child: const Text(
+                    "Validate",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    log("In dispose");
+    super.dispose();
   }
 }
