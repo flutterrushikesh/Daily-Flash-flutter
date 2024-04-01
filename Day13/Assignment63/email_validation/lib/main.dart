1c133cc3f03d3fe757a934dffed1c0ccb190c998
flutterrushikesh <flutterrushikesh@gmail.com>
2024-04-01 23:13:19 +0530

    Complete

--- Day13/Assignment63/email_validation/lib/main.dart
+++ Day13/Assignment63/email_validation/lib/main.dart
@@ -10,9 +10,86 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: EmailValidation(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class EmailValidation extends StatefulWidget {
+  const EmailValidation({super.key});
+  @override
+  State createState() => _EmailValidationState();
+}
+
+class _EmailValidationState extends State {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  String? emailError;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Email validation"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Form(
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: "Enter Email id",
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter an email address';
+                  } else if (!value.endsWith('@gmail.com')) {
+                    return 'Please enter a valid Gmail address';
+                  } else if (value.contains(' ')) {
+                    return 'Email address cannot contain spaces';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 19),
+              SizedBox(
+                height: 50,
+                width: 150,
+                child: ElevatedButton(
+                  onPressed: () {
+                    bool validateTextFeilds = formKey.currentState!.validate();
+                    if (validateTextFeilds) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text("Validation passed"),
+                        ),
+                      );
+                    } else {
+                      setState(() {
+                        emailError = emailController.text.isEmpty
+                            ? 'PhoneNo is required'
+                            : null;
+                      });
+                    }
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.blue),
+                    foregroundColor: MaterialStatePropertyAll(Colors.black),
+                  ),
+                  child: const Text(
+                    "Validate",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
